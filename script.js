37d35d39448b2d54c6bc5b4b8ff6a8afd386ca1d
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-06-22 19:36:04 +0200

    Finished functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const selectionBtns = document.querySelectorAll('[data-selection]');
+const resultColumn = document.querySelector('[data-result-column]');
+const yourScore = document.querySelector('[data-your-score]');
+const compScore = document.querySelector('[data-comp-score]');
 
 const SELECTIONS = [
     {
@@ -29,5 +32,40 @@ selectionBtns.forEach(selectionBtn => {
 });
 
 function makeSelection(selection) {
-    console.log(selection);
+    const compSelection = randomSelection();
+    // console.log(selection);
+    // console.log(compSelection);
+    const youAreWinner = isWinner(selection, compSelection);
+    const compIsWinner = isWinner(compSelection, selection);
+
+    addSelectionResult(compSelection, compIsWinner);
+    addSelectionResult(selection, youAreWinner);
+
+    if (youAreWinner) incrementScore(yourScore);
+    if (compIsWinner) incrementScore(compScore);
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div');
+
+    div.innerText = selection.emoji;
+    div.classList.add('past-result');
+
+    if (winner) div.classList.add('winner');
+
+    resultColumn.after(div);
+}
+
+function isWinner(selection, oponentsSelection) {
+    return selection.beats === oponentsSelection.name;
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+
+    return SELECTIONS[randomIndex];
 }
